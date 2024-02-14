d7c1db6fc04fce56190ceeb5e4a43b3a6e5c0f6b
Surendra Srinivas <83644731+Surendra-Srinivas@users.noreply.github.com>
2024-02-14 10:39:32 +0530

    Update Dataset_generation.m

--- Dataset_generation.m
+++ Dataset_generation.m
@@ -6,15 +6,17 @@ clc
 clear all
 
 pi=3.141592654;
-t_s = 1/3200;
+ts = 1/3200;
 t1_s = 0.00074844075;
 z=[]; cl=[]; X_train=[]; Y_train=[]; X_test=[]; Y_test=[];
+SNR = [10, 20, 30, 40, 100];
 
 %% Normal
+
 x = "Normal";
 t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
-for f = 49.5:0.02020:50.5    % 1250 different sine waves  (Runs 50 times)
-    for vm=0.97:0.0401606:1.03 % (Runs 25 times)
+for f = 49.5:0.025:50.5                % 1000 different sine waves  (Runs 40 times)
+    for vm=0.97:0.0401606:1.03         % (Runs 25 times)
     y = vm*sin(2*pi*f*t);
     y =awgn(y, SNR);
     z= vertcat(z,y);
@@ -40,7 +42,7 @@ L=length(z)+1;
 %% Sag
 
 x = "Sag";                            
-t = [0:ts:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0163:0.85              % Runs 50 times
@@ -78,7 +80,7 @@ L=length(z)+1;
 %% Swell
 
 x = "Swell";                            
-t = [0:ts:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.01428:0.8              % Runs 50 times
@@ -116,7 +118,7 @@ L=length(z)+1;
 %% Interruption
 
 x = "Interruption";                            
-t = [0:ts:0.2-ts];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.9:0.00204:1                 % Runs 50 times
@@ -154,7 +156,7 @@ L=length(z)+1;
 %% Harmonics
 
 x = "Harmonics";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha3=0.05:0.002038735:0.15           % Runs 50 times
@@ -184,7 +186,7 @@ L=length(z)+1;
 %% Flicker
 
 x = "Flicker";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha_flicker=0.06:0.003058109376:0.2    % Runs 50 times
@@ -213,7 +215,7 @@ L=length(z)+1;
 %% Oscillatory Transient
 
 x = "Oscillatory Transient";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0773480663:0.8                         % Runs 10 times
@@ -258,7 +260,7 @@ L=length(z)+1;
 %% Notch
 
 x = "Notch";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0006116207951:0.4;            % Runs 50 times
@@ -302,7 +304,7 @@ L=length(z)+1;
 %% Spike
 
 x = "Spike";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0006116207951:0.4;            % Runs 50 times
@@ -346,7 +348,7 @@ L=length(z)+1;
 %% Sag + Harmonics
 
 x = "Sag+Harmonics";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0888:0.9                         % Runs 10 times  
@@ -393,7 +395,7 @@ L=length(z)+1;
 %% Swell + Harmonics
 
 x = "Swell+Harmonics";                            
-t = [0:0.0003129:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0777:0.8                         % Runs 10 times  
@@ -440,7 +442,7 @@ L=length(z)+1;
 %% Interruption + Harmonics
 
 x = "Interruption+Harmonics";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.9:0.0111:1                           % Runs 10 times  
@@ -488,7 +490,7 @@ L=length(z)+1;
 %% Flicker + Harmonics
 
 x = "Flicker+Harmonics";                            
-t = [0:t_s:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha_flicker=0.08:0.01104972376:0.2                 % Runs 10 times
@@ -522,7 +524,7 @@ L=length(z)+1;
 %% Flicker + Sag
 
 x = "Flicker+Sag";                            
-t = [0:0.0003129:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.088:0.9                          % Runs 10 times  
@@ -567,7 +569,7 @@ L=length(z)+1;
 %% Flicker + Swell
 
 x = "Flicker+Swell";                            
-t = [0:0.0003129:0.2];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.077:0.8                          % Runs 10 times  
@@ -613,7 +615,7 @@ L=length(z)+1;
 %% Osciallatory + Sag
 
 x = "Oscillatory Transient + Sag";                            
-t = [0:t_s:0.2-t_s];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0773480663:0.9                         % Runs 10 times
@@ -659,7 +661,7 @@ L=length(z)+1;
 %% Osciallatory + Swell
 
 x = "Oscillatory Transient + Swell ";                            
-t = [0:t_s:0.2-t_s];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0773480663:0.9                         % Runs 10 times
@@ -705,7 +707,7 @@ L=length(z)+1;
 %% Oscillatory Transients + Interruptions
 
 x = "Oscillatory Transient + Interruptions";                            
-t = [0:t_s:0.2-t_s];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0773480663:0.9                         % Runs 10 times
@@ -752,7 +754,7 @@ L=length(z)+1;
 % Ask Sir
 
 x = "Oscillatory Transient + Harmonics";                            
-t = [0:t_s:0.2-t_s];
+t = [0: ts :0.2-ts];                   % 640 sample points per disturbance
 f = 50;
 
 for alpha=0.1:0.0773480663:0.9                         % Runs 10 times
