5197cf89faf487e5c7541bb943f72ebfcc417b92
AbhinavJ07 <83644974+AbhinavJ07@users.noreply.github.com>
2024-03-07 11:15:00 +0530

    Transients and Harmonics.py

--- Model.py
+++ Model.py
@@ -6,8 +6,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
 from tensorflow.keras.utils import to_categorical
 
-num_classes = 7
-classes = ['Normal', 'Sag', 'Swell', 'Interruption', 'Notch', 'Spike', 'Flicker']
+num_classes = 9
+classes = ['Normal', 'Sag', 'Swell', 'Interruption', 'Notch', 'Spike', 'Flicker', 'Oscillatory Transients', 'Harmonics']
 
 Sine_data = scipy.io.loadmat('Sine_data.mat')
 X_Sine_train = Sine_data['sine_train']
@@ -79,6 +79,27 @@ y_Flicker_train = Flicker_label_data['flicker_label_train']
 y_Flicker_val = Flicker_label_data['flicker_label_val']
 y_Flicker_test = Flicker_label_data['flicker_label_test']
 
+Oscillatory_Transients_data = scipy.io.loadmat('Oscillatory_Transients_data.mat')
+X_Oscillatory_Transients_train = Oscillatory_Transients_data['oscillatory_transients_train']
+X_Oscillatory_Transients_val = Oscillatory_Transients_data['oscillatory_transients_val']
+X_Oscillatory_Transients_test = Oscillatory_Transients_data['oscillatory_transients_test']
+
+Oscillatory_Transients_label_data = scipy.io.loadmat('Oscillatory_Transients_label_data.mat')
+y_Oscillatory_Transients_train = Oscillatory_Transients_label_data['Oscillatory_Transients_label_train']
+y_Oscillatory_Transients_val = Oscillatory_Transients_label_data['Oscillatory_Transients_label_val']
+y_Oscillatory_Transients_test = Oscillatory_Transients_label_data['Oscillatory_Transients_label_test']
+
+Harmonics_data = scipy.io.loadmat('Harmonics_data.mat')
+X_Harmonics_train = Harmonics_data['harmonics_train']
+X_Harmonics_val = Harmonics_data['harmonics_val']
+X_Harmonics_test = Harmonics_data['harmonics_test']
+
+Harmonics_label_data = scipy.io.loadmat('Harmonics_label_data.mat')
+y_Harmonics_train = Harmonics_label_data['harmonics_label_train']
+y_Harmonics_val = Harmonics_label_data['harmonics_label_val']
+y_Harmonics_test = Harmonics_label_data['harmonics_label_test']
+
+
 # Convert lists of arrays to numpy arrays
 X_Sine_train = np.array([np.vstack(chunk) for chunk in X_Sine_train])
 X_Sine_val = np.array([np.vstack(chunk) for chunk in X_Sine_val])
@@ -142,20 +163,37 @@ y_Flicker_train = y_Flicker_train.reshape(np.shape(X_Flicker_train)[0])
 y_Flicker_test = y_Flicker_test.reshape(np.shape(X_Flicker_test)[0])
 y_Flicker_val = y_Flicker_val.reshape(np.shape(X_Flicker_val)[0])
 
+# Convert lists of arrays to numpy arrays
+X_Oscillatory_Transients_train = np.array([np.vstack(chunk) for chunk in X_Oscillatory_Transients_train])
+X_Oscillatory_Transients_val = np.array([np.vstack(chunk) for chunk in X_Oscillatory_Transients_val])
+X_Oscillatory_Transients_test = np.array([np.vstack(chunk) for chunk in X_Oscillatory_Transients_test])
 
-X_train = np.concatenate([X_Sine_train, X_Sag_train, X_Swell_train, X_Interruption_train, X_Notch_train, X_Spike_train, X_Flicker_train], axis = 0)
-y_train = np.concatenate([y_Sine_train, y_Sag_train, y_Swell_train, y_Interruption_train, y_Notch_train, y_Spike_train, y_Flicker_train], axis = 0)
+y_Oscillatory_Transients_train = y_Oscillatory_Transients_train.reshape(np.shape(X_Oscillatory_Transients_train)[0])
+y_Oscillatory_Transients_test = y_Oscillatory_Transients_test.reshape(np.shape(X_Oscillatory_Transients_test)[0])
+y_Oscillatory_Transients_val = y_Oscillatory_Transients_val.reshape(np.shape(X_Oscillatory_Transients_val)[0])
 
-X_test = np.concatenate([X_Sine_test, X_Sag_test, X_Swell_test, X_Interruption_test, X_Notch_test, X_Spike_test, X_Flicker_test], axis = 0)
-y_test = np.concatenate([y_Sine_test, y_Sag_test, y_Swell_test, y_Interruption_test, y_Notch_test, y_Spike_test, y_Flicker_test], axis = 0)
+# Convert lists of arrays to numpy arrays
+X_Harmonics_train = np.array([np.vstack(chunk) for chunk in X_Harmonics_train])
+X_Harmonics_val = np.array([np.vstack(chunk) for chunk in X_Harmonics_val])
+X_Harmonics_test = np.array([np.vstack(chunk) for chunk in X_Harmonics_test])
 
-X_val = np.concatenate([X_Sine_val, X_Sag_val, X_Swell_val, X_Interruption_val, X_Notch_val, X_Spike_val, X_Flicker_val], axis = 0)
-y_val = np.concatenate([y_Sine_val, y_Sag_val, y_Swell_val, y_Interruption_val, y_Notch_val, y_Spike_val, y_Flicker_val], axis = 0)
+y_Harmonics_train = y_Harmonics_train.reshape(np.shape(X_Harmonics_train)[0])
+y_Harmonics_test = y_Harmonics_test.reshape(np.shape(X_Harmonics_test)[0])
+y_Harmonics_val = y_Harmonics_val.reshape(np.shape(X_Harmonics_val)[0])
+
+
+X_train = np.concatenate([X_Sine_train, X_Sag_train, X_Swell_train, X_Interruption_train, X_Notch_train, X_Spike_train, X_Flicker_train, X_Oscillatory_Transients_train, X_Harmonics_train], axis = 0)
+y_train = np.concatenate([y_Sine_train, y_Sag_train, y_Swell_train, y_Interruption_train, y_Notch_train, y_Spike_train, y_Flicker_train, y_Oscillatory_Transients_train, y_Harmonics_train], axis = 0)
+
+X_test = np.concatenate([X_Sine_test, X_Sag_test, X_Swell_test, X_Interruption_test, X_Notch_test, X_Spike_test, X_Flicker_test, X_Oscillatory_Transients_test, X_Harmonics_test], axis = 0)
+y_test = np.concatenate([y_Sine_test, y_Sag_test, y_Swell_test, y_Interruption_test, y_Notch_test, y_Spike_test, y_Flicker_test, y_Oscillatory_Transients_test, y_Harmonics_test], axis = 0)
+
+X_val = np.concatenate([X_Sine_val, X_Sag_val, X_Swell_val, X_Interruption_val, X_Notch_val, X_Spike_val, X_Flicker_val, X_Oscillatory_Transients_val, X_Harmonics_val], axis = 0)
+y_val = np.concatenate([y_Sine_val, y_Sag_val, y_Swell_val, y_Interruption_val, y_Notch_val, y_Spike_val, y_Flicker_val, y_Oscillatory_Transients_val, y_Harmonics_val], axis = 0)
 
 y_train = to_categorical(y_train, num_classes = num_classes)
 y_test = to_categorical(y_test, num_classes = num_classes)
 y_val = to_categorical(y_val, num_classes = num_classes)
-
 
 print('The shape of X_Sine_train is : ',np.shape(X_Sine_train))
 print('The shape of X_Sag_train is : ',np.shape(X_Sag_train))
@@ -164,6 +202,8 @@ print('The shape of X_Interruption_train is : ',np.shape(X_Interruption_train))
 print('The shape of X_Notch_train is : ',np.shape(X_Notch_train))
 print('The shape of X_Spike_train is : ',np.shape(X_Spike_train))
 print('The shape of X_Flicker_train is : ',np.shape(X_Flicker_train))
+print('The shape of X_Oscillatory_Transients_train is : ',np.shape(X_Oscillatory_Transients_train))
+print('The shape of X_Harmonics_train is : ',np.shape(X_Harmonics_train))
 print("\n")
 print('The shape of y_Sine_train is : ',np.shape(y_Sine_train))
 print('The shape of y_Sag_train is : ',np.shape(y_Sag_train))
@@ -172,6 +212,8 @@ print('The shape of y_Interruption_train is : ',np.shape(y_Interruption_train))
 print('The shape of y_Notch_train is : ',np.shape(y_Notch_train))
 print('The shape of y_Spike_train is : ',np.shape(y_Spike_train))
 print('The shape of y_Flicker_train is : ',np.shape(y_Flicker_train))
+print('The shape of y_Oscillatory_Transients_train is : ',np.shape(y_Oscillatory_Transients_train))
+print('The shape of y_Harmonics_train is : ',np.shape(y_Harmonics_train))
 print("\n")
 print("The shape of X_train is : ", np.shape(X_train))
 print("The shape of y_train is : ", np.shape(y_train))
